36c163f265a4b320f14b4511567602306c57fca8
GodswillOJ <110397088+GodswillOJ@users.noreply.github.com>
2024-11-14 06:52:06 +0100

    bkjb

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,8 +1,28 @@
+import HeaderBox from '@/components/ui/headerBox'
 import React from 'react'
-
+import TotalBalanceBox from '@/components/ui/totalBalanceBox'
 const Home = () => {
+  const loggedIn = { first_name: "God'swill" }
+
   return (
-    <div>home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.first_name || "guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
